11b5d13f0e37b2b163eb3a7562d2d8b979dd9b03
Leytto <vitel@MacBook-Pro-de-Loic-3.local>
2016-05-09 21:08:55 +0200

    Ajout de la base du projet
    Structure du projet créé avec Libgdx. Le programme sera créé dans le
    core afin de pouvoir être compilé sur desktop également

---
+++ Project/android/src/ch/iglwars/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ch.iglwars;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ch.iglwars.IGLWars;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new IGLWars(), config);
+	}
+}
---
+++ Project/desktop/src/ch/iglwars/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ch.iglwars.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ch.iglwars.IGLWars;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new IGLWars(), config);
+	}
+}
