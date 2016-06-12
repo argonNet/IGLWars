2e363a3ecc4a8062fbd3fb4d254178f466f4bc4f
jPerritaz <j.perritaz@panipro.ch>
2016-06-12 15:05:20 +0200

    - Ajout du scrolling automatique de background du jeu

--- Project/core/src/ch/iglwars/Screen/GameScreen.java
+++ Project/core/src/ch/iglwars/Screen/GameScreen.java
@@ -2,18 +2,22 @@ package ch.iglwars.Screen;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.ui.TextField;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+import com.rahul.libgdx.parallax.ParallaxBackground;
+import com.rahul.libgdx.parallax.ParallaxLayer;
+import com.rahul.libgdx.parallax.TextureRegionParallaxLayer;
+import com.rahul.libgdx.parallax.Utils;
 
 import ch.iglwars.Utils.Constants;
 import ch.iglwars.IGLWars;
@@ -27,10 +31,13 @@ public class GameScreen implements Screen {
     final IGLWars game;
     private Viewport viewport;
     private OrthographicCamera camera;
+    private OrthographicCamera backgroundCamera;
 
     private int score = 0;
 
     private Level1 level1;
+    //Permet de faire un mouvement automatique pour le fond avec optimisation
+    private ParallaxBackground parallaxBackground;
 
 
     /**
@@ -59,21 +66,51 @@ public class GameScreen implements Screen {
         return   txtUsername.getText();
     }
 
+    /**
+     * Créer l'image de fond qui défilera tout au long du jeu
+     * Utilisation de la librairie créée par Surahul:
+     * https://github.com/surahul/ParallaxBackground-libgdx
+     */
+    private void createBackgroundLayers() {
+
+        Texture landTexture = new Texture(Gdx.files.internal("backgroundLand.png"));
+        TextureRegion land = new TextureRegion(landTexture);
+        TextureRegionParallaxLayer landLayer = new TextureRegionParallaxLayer(land, Constants.GAME_HEIGHT, new Vector2(.3f,.3f), Utils.WH.height);
+        landLayer.setTileModeY(ParallaxLayer.TileMode.repeat);
+
+        Texture cloudTexture = new Texture(Gdx.files.internal("backgroundCloud.png"));
+        TextureRegion cloud = new TextureRegion(cloudTexture);
+        TextureRegionParallaxLayer cloudLayer = new TextureRegionParallaxLayer(cloud, Constants.GAME_HEIGHT * .6f, new Vector2(.9f,.9f), Utils.WH.height);
+        cloudLayer.setTileModeY(ParallaxLayer.TileMode.repeat);
+        cloudLayer.setPadBottom(Constants.GAME_HEIGHT*.1f);
+
+        parallaxBackground = new ParallaxBackground();
+        parallaxBackground.addLayers(landLayer,cloudLayer);
+
+
+    }
+
     public GameScreen(final IGLWars game) {
         this.game = game;
         camera = new OrthographicCamera();
         camera.setToOrtho(false, Constants.GAME_WIDTH, Constants.GAME_HEIGHT);
-        viewport = new FitViewport(Constants.GAME_WIDTH, Constants.GAME_HEIGHT, camera);
+        backgroundCamera = new OrthographicCamera();
+        backgroundCamera.setToOrtho(false, Constants.GAME_WIDTH, Constants.GAME_HEIGHT);
 
+        viewport = new FitViewport(Constants.GAME_WIDTH, Constants.GAME_HEIGHT, camera);
+        createBackgroundLayers();
         level1 = new Level1();
     }
+
 
     /**
      * Gestion de resize du viewport (pour une meilleure gestion des différentes tailles d'écran)
      */
     @Override
     public void resize(int width, int height) {
+
         viewport.update(width, height);
+       // viewport2.update(width, height);
     }
 
     /**
@@ -82,16 +119,21 @@ public class GameScreen implements Screen {
     @Override
     public void render (float delta) {
         //Fond noir par défaut
-        Gdx.gl.glClearColor(1, 1, 1, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 
-        //Définition de la caméra (en 2D)
-        camera.update();
-        game.getBatch().setProjectionMatrix(camera.combined);
-
         //début du mode de jeu
         game.getBatch().begin();
+
+        //Définition de la caméra du background
+        backgroundCamera.position.add(0, 1f, 0);
+        backgroundCamera.update();
+        parallaxBackground.draw(backgroundCamera, game.getBatch());
+
+        //Définition de la caméra (en 2D)
+        game.getBatch().setProjectionMatrix(camera.combined);
+        camera.update();
 
         if (level1.isEnded()) {
             game.setScreen(new GameOverScreen(game));
